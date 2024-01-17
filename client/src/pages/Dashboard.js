3dc290cc3cbec6386223ac96704d9a8131d6b8b2
nickbakeruvic <106908272+nickbakeruvic@users.noreply.github.com>
2024-01-17 12:49:41 -0800

    Dashboard: code formatting updates.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -52,23 +52,43 @@ const Dashboard = () => {
 
 	return (
 		<>
-			<div className="add-button-wrapper" id="button-wrapper" onClick={(e) => setAddingJournal(true)}>
-				<img src={ add_icon } className="icon" alt="Add journal icon"></img>
-				<span className="button-text">Add Journal</span>
+			<div className="add-button-wrapper" id="button-wrapper" onClick={() => setAddingJournal(true)}>
+				<img
+					src={ add_icon }
+					className="icon"
+					alt="Add journal icon">
+				</img>
+				<span className="button-text">
+					Add Journal
+				</span>
 			</div>
+
 			<div className="delete-button-wrapper" id="button-wrapper" onClick={() => logout()}>
-				<img src={ logout_icon } className="icon" alt="Logout icon"></img>
-				<span className="button-text">Logout</span>
+				<img
+					src={ logout_icon }
+					className="icon"
+					alt="Logout icon">
+				</img>
+				<span className="button-text">
+					Logout
+				</span>
 			</div>
+
+
+
 			{addingJournal &&
-			<Journal_Editor
+			<JournalEditor
 				journal={ {title: '', content: '', _id: '', new_journal: true } }
 				exit_callback={ () => setAddingJournal(false) }
 				populate_callback={ populateJournals }
 			/>}
-			<div>
+			<div className="dashboard-title-wrapper">
+				<span>Journal App</span>
+			</div>
+
+			<div className="dashboard-item-wrapper">
 				{journals.map(journal => (
-					<Journal_Item
+					<JournalItem
 						key={ journal._id }
 						journal={ journal }
 						populate_callback={ populateJournals }
@@ -79,7 +99,7 @@ const Dashboard = () => {
 	)
 }
 
-function Journal_Item({journal, populate_callback}) {
+function JournalItem({journal, populate_callback}) {
 	const [editingJournal, setEditingJournal] = useState(false)
 
 	const weekdays = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
@@ -93,41 +113,59 @@ function Journal_Item({journal, populate_callback}) {
 	return (
 		<>
 			{editingJournal &&
-				<Journal_Editor
+				<JournalEditor
 					journal={ journal }
 					exit_callback={ () => setEditingJournal(false) }
 					populate_callback={ populate_callback }
 				/>}
+
 			<div className="preview-wrapper">
 				<div className='preview-date-wrapper' id="editor-column">
-					<img src={ calendar_icon } className="icon" alt="Calendar icon"></img>
-					<span className="button-text"> { weekdays[day] + ", " + months[monthNum]+  " " + dayNum } </span>
+					<img
+						src={ calendar_icon }
+						className="icon"
+						alt="Calendar icon">
+					</img>
+					<span className="button-text">
+						{ weekdays[day] + ", " + months[monthNum] +  " " + dayNum }
+					</span>
 				</div>
+
 				<div className='preview-title-wrapper' id="editor-column">
-					<span> { journal.title } </span>
+					<span>
+						{ journal.title }
+					</span>
 				</div>
+
 				<div className='preview-content-wrapper' id="editor-column">
-					<span> { journal.content } </span>
+					<span>
+						{ journal.content }
+					</span>
 				</div>
-				<div className="preview-edit-wrapper" id="editor-column" onClick={(e) => setEditingJournal(true)}>
-					<img src={ edit_icon } className="icon" alt="Edit icon"></img>
-					<span className="button-text">Edit</span>
+
+				<div className="preview-edit-wrapper" id="editor-column" onClick={() => setEditingJournal(true)}>
+					<img
+						src={ edit_icon }
+						className="icon"
+						alt="Edit icon">
+					</img>
+					<span className="button-text">
+						Edit
+					</span>
 				</div>
+
 			</div>
 		</>
 	);
 }
 
-function Journal_Editor({journal, exit_callback, populate_callback}) {
+function JournalEditor({journal, exit_callback, populate_callback}) {
 	const [newTitle, setNewTitle] = useState(journal.title)
 	const [newContent, setNewContent] = useState(journal.content)
 	const deleteIconMessage = journal.new_journal ? 'Discard' : 'Delete'
 
-	async function updateJournal(event) {
+	async function updateJournal() {
 		let http_method = 'PUT'
-
-		if (event !== null)
-			event.preventDefault()
 
 		if (journal.title === newTitle && journal.content === newContent)
 			return
@@ -187,7 +225,9 @@ function Journal_Editor({journal, exit_callback, populate_callback}) {
 					onChange={(e) => setNewTitle(e.target.value)}
 				/>
 			</div>
+
 			<hr></hr>
+
 			<div className="journal-interactions-wrapper">
 				<div className="journal-content-wrapper">
 					<textarea
@@ -196,25 +236,48 @@ function Journal_Editor({journal, exit_callback, populate_callback}) {
 						value={newContent}
 						onChange={(e) => setNewContent(e.target.value)}
 					/>
+
 					<div className="spacer">
-						<div className="save-button-wrapper" onClick={(e) => {updateJournal(null); populate_callback(); exit_callback()} }>
-							<img src={ save_icon } className="save-icon" alt="Save journal icon" ></img>
-							<span className="button-text">Save & Exit</span>
+						<div className="save-button-wrapper" onClick={() => {updateJournal(null); populate_callback(); exit_callback()} }>
+							<img
+								src={ save_icon }
+								className="save-icon"
+								alt="Save journal icon" >
+							</img>
+							<span className="button-text">
+								Save & Exit
+							</span>
 						</div>
 					</div>
-
 				</div>
 			</div>
-			<div className="delete-button-wrapper" id="button-wrapper" onClick={(e) => {deleteJournal(); populate_callback(); exit_callback()}}>
-				<img src={ delete_icon } className="icon" alt="Delete journal icon"></img>
-				<span className="button-text">{ deleteIconMessage }</span>
+
+			<div className="delete-button-wrapper" id="button-wrapper" onClick={() => {deleteJournal(); populate_callback(); exit_callback()}}>
+				<img
+					src={ delete_icon }
+					className="icon"
+					alt="Delete journal icon">
+				</img>
+				<span className="button-text">
+					{ deleteIconMessage }
+				</span>
 			</div>
-			<div className="back-button-wrapper" id="button-wrapper" onClick={(e) => exit_callback()}>
-				<img src={ back_icon } className="icon" alt="Back arrow icon"></img>
-				<span className="button-text">Back</span>
+
+			<div className="back-button-wrapper" id="button-wrapper" onClick={() => exit_callback()}>
+				<img
+					src={ back_icon }
+					className="icon"
+					alt="Back arrow icon">
+				</img>
+				<span className="button-text">
+					Back
+				</span>
 			</div>
+
 		</div>
 	);
 }
+
+/* deleteJournalButton component etc */
 
 export default Dashboard
